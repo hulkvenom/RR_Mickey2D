0281782a0dd68857dae3d0296c6ab7f8b022a91e
hulkvenom <115841729+hulkvenom@users.noreply.github.com>
2023-11-06 15:29:48 -0600

    movement

--- Mickey2D/Assets/_MyFiles/Scripts/PlayerMovement.cs
+++ Mickey2D/Assets/_MyFiles/Scripts/PlayerMovement.cs
@@ -1,9 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    PlayerControls controls;
+    float direction = 0;
+    public float speed = 200f;
+    bool isFacingRight = true;
+    public Rigidbody2D playerRB;
+    public Animator animator;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Enable();
+
+        controls.Movement.Move.performed += ctx =>
+        {
+            direction = ctx.ReadValue<float>();
+        };
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +33,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerRB.velocity = new Vector2(direction * speed * Time.deltaTime, playerRB.velocity.y);
+        animator.SetFloat("speed", Mathf.Abs(direction));
+
+        if(isFacingRight && direction <0 || !isFacingRight && direction >0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
     }
 }
