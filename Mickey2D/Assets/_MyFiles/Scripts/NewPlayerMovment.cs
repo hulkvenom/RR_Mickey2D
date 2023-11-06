b6bf47033388000f8341740bf450ebcb76cdbbaa
hulkvenom <115841729+hulkvenom@users.noreply.github.com>
2023-11-06 11:08:42 -0600

    almost success

---
+++ Mickey2D/Assets/_MyFiles/Scripts/NewPlayerMovment.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerMovment : MonoBehaviour
+{
+    private float horizontal;
+    private float speed;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        Debug.Log(horizontal);
+
+        if(Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontal <0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
--- Mickey2D/Assets/_MyFiles/Scripts/PlayerMovement.cs
+++ Mickey2D/Assets/_MyFiles/Scripts/PlayerMovement.cs
@@ -2,61 +2,131 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 15f;
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private float m_JumpForce = 400f;                       
+    [Range(0, 1)][SerializeField] private float m_CrouchSpeed = .36f;          
+    [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;   
+    [SerializeField] private bool m_AirControl = false;                      
+    [SerializeField] private LayerMask m_WhatIsGround;                          
+    [SerializeField] private Transform m_GroundCheck;                         
+    [SerializeField] private Transform m_CeilingCheck;                         
+    [SerializeField] private Collider2D m_CrouchDisableCollider;                
 
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
+    const float k_GroundedRadius = .2f; 
+    private bool m_Grounded;           
+    const float k_CeilingRadius = .2f; 
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;  
+    private Vector3 m_Velocity = Vector3.zero;
+
+    //[Header("Events")]
+    //[Space]
+
+    //public UnityEvent OnLandEvent;
+
+    //[System.Serializable]
+    //public class BoolEvent : UnityEvent<bool> { }
+
+    //public BoolEvent OnCrouchEvent;
+    //private bool m_wasCrouching = false;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+        //if (OnLandEvent == null)
+        //    OnLandEvent = new UnityEvent();
+
+        //if (OnCrouchEvent == null)
+        //    OnCrouchEvent = new BoolEvent();
+    }
+  
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = m_Grounded;
+        m_Grounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        //for (int i = 0; i < colliders.Length; i++)
+        //{
+        //    if (colliders[i].gameObject != gameObject)
+        //    {
+        //        m_Grounded = true;
+        //        if (!wasGrounded)
+        //            OnLandEvent.Invoke();
+        //    }
+        //}
     }
 
-    private void Update()
-    {
-        GroundCheck();
-        Move();
-    }
 
-    private void OnMove(InputValue movementValue)
+    public void Move(float move, bool crouch, bool jump)
     {
-        moveInput = movementValue.Get<Vector2>();
-    }
+        //if (!crouch)
+        //{
+        //    if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+        //    {
+        //        crouch = true;
+        //    }
+        //}
 
-    private void OnJump(InputValue value)
-    {
-        if (isGrounded)
+        if (m_Grounded || m_AirControl)
         {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
+            //if (crouch)
+            //{
+            //    if (!m_wasCrouching)
+            //    {
+            //        m_wasCrouching = true;
+            //        OnCrouchEvent.Invoke(true);
+            //    }
+
+            //    move *= m_CrouchSpeed;
+
+            //    if (m_CrouchDisableCollider != null)
+            //        m_CrouchDisableCollider.enabled = false;
+            //}
+            //else
+            //{
+            //    if (m_CrouchDisableCollider != null)
+            //        m_CrouchDisableCollider.enabled = true;
+
+            //    if (m_wasCrouching)
+            //    {
+            //        m_wasCrouching = false;
+            //        OnCrouchEvent.Invoke(false);
+            //    }
+            //}
+
+            Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+            if (move > 0 && !m_FacingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && m_FacingRight)
+            {
+                Flip();
+            }
+        }
+        if (m_Grounded && jump)
+        {
+            m_Grounded = false;
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
         }
     }
 
-    private void Move()
-    {
-        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
-    }
 
-    private void GroundCheck()
+    private void Flip()
     {
-        isGrounded = false;
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundCheckRadius, groundLayer);
-        if (colliders.Length > 0)
-        {
-            isGrounded = true;
-        }
-    }
+        m_FacingRight = !m_FacingRight;
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
